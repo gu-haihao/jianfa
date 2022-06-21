ce1a856c41a926d6e77dba7375137905e49e6e0a
weiyunhui <weiyunhui@atguigu.com>
2022-06-21 14:32:04 +0800

    test

---
+++ src/main/java/com/atguigu/git/Test.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName：Test
+ * @Author: 八宝粥
+ * @Date: 2022/6/21 14:30
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
